a65468c50456ed05a2e77b9c74a7cff1625775ad
LAPTOP-JV7IVMTC\gustj <gustjr1259@naver.com>
2019-07-25 17:33:33 +0900

    sd

--- app_with_handler.py
+++ app_with_handler.py
@@ -68,7 +68,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
+    logging.error(body)
     # handle webhook body
     try:
         handler.handle(body, signature)
