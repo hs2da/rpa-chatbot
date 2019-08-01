58941c6e5e1b0cf7ed155c51cfb9c51c264cdc4d
LAPTOP-JV7IVMTC\gustj <gustjr1259@naver.com>
2019-08-01 16:30:36 +0900

    한글 지원

--- app_with_handler.py
+++ app_with_handler.py
@@ -67,13 +67,13 @@ def create_sha256_signature(message, key):
 url = "http://46ec6ee9.ngrok.io/automateone/api/v1/runProcessWithDataset"
 accessToken = "test"
 secretKey = "098F6BCD4621D373CADE4E832627B4F6"
-contents = '{"projectId": 1, "processId": 2, "dataset":{"ID":"Uce275b6ee9ce7f001a4540c74e1304fa","Message":"success"}}'
+contents = '{"projectId": 1, "processId": 2, "dataset":{"ID":"Uce275b6ee9ce7f001a4540c74e1304fa","Message":"예약이 완료되었습니다."}}'
 nonce = str(time.time())
 payload = url + '\n' + accessToken + '\n' + nonce + '\n' + contents + '\n'
 signatureBytes = create_sha256_signature(payload, secretKey)
 #signatureBase64String = base64.b64encode(signatureBytes)
 authorization = accessToken + ":" + nonce + ":" + signatureBytes
-headers = {'content-type': 'application/json'}
+headers = {'content-type': 'application/json; charset=utf-8'}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -100,7 +100,7 @@ def message_text(event):
         event.reply_token,
         TextSendMessage(text="예약 진행중입니다")
     )
-    r = requests.post(url, data=contents, headers=headers)
+    r = requests.post(url, data=contents.encode('utf-8'), headers=headers)
 
 def testReply(RP,txt):
     line_bot_api.push_message(
