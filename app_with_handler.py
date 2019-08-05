4c2424d866fce77c62e4b048410d4cabe256d7e3
LAPTOP-JV7IVMTC\gustj <gustjr1259@naver.com>
2019-08-05 14:10:29 +0900

    테스트

--- app_with_handler.py
+++ app_with_handler.py
@@ -64,7 +64,7 @@ def create_sha256_signature(message, key):
     message = message.encode()
     return hmac.new(byte_key, message, hashlib.sha256).hexdigest().upper()
 
-url = "http://b2781c2b.ngrok.io/automateone/api/v1/runProcessWithDataset"
+url = "http://de32ee4d.ngrok.io/automateone/api/v1/runProcessWithDataset"
 accessToken = "test"
 secretKey = "098F6BCD4621D373CADE4E832627B4F6"
 contents = '{"projectId": 1, "processId": 2, "dataset":{"ID":"Uce275b6ee9ce7f001a4540c74e1304fa","Message":"success" }}'
