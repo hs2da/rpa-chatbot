3f0da96b22b151acdd7f6d24d572a6b60ee16a80
LAPTOP-JV7IVMTC\gustj <gustjr1259@naver.com>
2019-08-20 13:34:08 +0900

    테스트

--- app_with_handler.py
+++ app_with_handler.py
@@ -74,7 +74,8 @@ payload = url + '\n' + accessToken + '\n' + nonce + '\n' + contents + '\n'
 signatureBytes = create_sha256_signature(payload, secretKey)
 #signatureBase64String = base64.b64encode(signatureBytes)
 authorization = accessToken + ":" + nonce + ":" + signatureBytes
-headers = {"content-type": "text/json; charset=UTF-8"}
+headers = {"content-type": "application/json; charset=UTF-8"}
+contents.encode('utf-8')
 
 @app.route("/callback", methods=['POST'])
 def callback():
