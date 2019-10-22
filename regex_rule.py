3e8bd3c4c4b45c36c8bbb05386fb90e48b93e5c8
LAPTOP-JV7IVMTC\gustj <gustjr1259@naver.com>
2019-10-22 15:29:53 +0900

    수정중

--- regex_rule.py
+++ regex_rule.py
@@ -1,10 +1,10 @@
 import re
 
 url = "http://9327549e.ngrok.io/automateone/api/v1/runProcessWithDataset"
-accessToken = "test"
-secretKey = "098F6BCD4621D373CADE4E832627B4F6"
+#accessToken = "test"
+#secretKey = "098F6BCD4621D373CADE4E832627B4F6"
 contents = {"projectId": 1, "processId": 2, "dataset":{"ID":"Uce275b6ee9ce7f001a4540c74e1304fa","Message":"success" }}
-nonce = str(time.time())
+#nonce = str(time.time())
 #payload = url + '\n' + accessToken + '\n' + nonce + '\n' + contents + '\n'
 #signatureBytes = create_sha256_signature(payload, secretKey)
 #signatureBase64String = base64.b64encode(signatureBytes)
